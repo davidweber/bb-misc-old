a14d4ff8d48214b0f450de86d84af3d889309c5d
David Weber <david@david-weber.com>
2011-09-11 11:15:15 -0700

    added files

---
+++ apps/helloworld/helloworld.c
@@ -0,0 +1,32 @@
+/**
+ * @file helloworld.c
+ * @author David Weber <david@david-weber.com>
+ * @date Wed Aug 10 2011 15:40:50
+ *
+ * @brief Hello World application
+ *
+ * This file  print Hello World on the stdout.
+ * 
+ * Copyright (C) David Weber. Unpublished
+ * 
+ * Proprietary & Confidential
+ * 
+ * This file contains confidential information and may compromise trade secrets 
+ * of David Weber and/or associates, and any unauthorized use thereof is
+ * prohibited.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+//------------------------------------------------------------------------------
+
+/**
+ * This function prints "Hello World!"
+ */
+int main(int argc, char* argv[]) 
+{
+  printf("Hello World!\n");
+  return 0;
+}
+
